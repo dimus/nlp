a574047c19ddedbbdc1a099116baddbf0560c45d
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-11-07 19:42:33 +0000

    embed big text

--- sent-emb.py
+++ sent-emb.py
@@ -14,36 +14,41 @@ def split_text_into_chunks(text, chunk_size=1000, overlap=50):
 
     return chunks
 
-# Initializing a SentenceTransformer model with the 'multi-qa-mpnet-base-cos-v1'
-# pre-trained model
 
-# model = SentenceTransformer('all-MiniLM-L6-v2' )
-model = SentenceTransformer('multi-qa-mpnet-base-cos-v1' )
+# Initializing a SentenceTransformer model with the
+# 'multi-qa-mpnet-base-cos-v1' pre-trained model
+model = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 
 # Defining a list of documents to generate embeddings for
 docs = [
-"Around 9 million people live in London",
-"London is known for its financial district"
+    "Around 9 million people live in London",
+    "London is known for its financial district"
 ]
 
 # Generate vector embeddings for the documents
 
 doc_emb = model.encode(
-docs, # Our documents (an iterable of strings)
-batch_size=32, # Batch the embeddings by this size
-show_progress_bar=True # Display a progress bar
+    docs,  # Our documents (an iterable of strings)
+    batch_size=32,  # Batch the embeddings by this size
+    show_progress_bar=True  # Display a progress bar
 )
 
 # The shape of the embeddings is (2, 768), indicating a length of 768 and two
 # embeddings generated
 
-print(doc_emb.shape) # == (2, 768)
+print(doc_emb.shape)  # == (2, 768)
 
 with open('./data/seashells_book.txt', 'r') as file:
     txt = file.read()
 
 chunks = split_text_into_chunks(txt)
 
-for i, chunk in enumerate(chunks):
-   print(f"Chunk {i + 1}:\n{chunk}\n\n\n")
+doc_emb = model.encode(
+    chunks,
+    batch_size=32,
+    show_progress_bar=True
+)
 
+print(doc_emb.shape)  # == (2, 768)
+# for i, chunk in enumerate(chunks):
+#     print(f"Chunk {i + 1}:\n{chunk}\n\n\n")
